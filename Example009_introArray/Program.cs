08f2a1bdaf7307ef02743224bc986681883cdf6f
Oksana Oreshkina <jani93@mail.ru>
2023-01-30 19:59:11 +0300

    add lekcia 2

---
+++ Example009_introArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {11,211,31,41,15,16,17,18,19};
+
+array[0] = 12;  // заменить значение с индексом 0 на 12.
+
+int result = Max(  
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8])
+);
+
+System.Console.WriteLine(result);
---
+++ Examples011_ArrayLibrary/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)    // подставляет рандомные значения от 1 до 10 в массив
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)  // выводит на экран значение каждой позиции рандомного массива
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.Write(col[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) // ищет индекс заданного числа в массиве (первое вхождение). Если элемента в массиве нет выведет -1
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];  //создает массив из 10 элементов, которые заполнены 0 если иные значения не указаны
+
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+System.Console.WriteLine(pos);
