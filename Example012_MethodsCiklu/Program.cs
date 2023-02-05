091b8a4adfea6ee21f5ea4a0656cfc1b5a255972
Oksana Oreshkina <jani93@mail.ru>
2023-02-05 11:46:35 +0300

    add lekciya 04/02/2023

---
+++ Example012_MethodsCiklu/Program.cs
@@ -0,0 +1,152 @@
+﻿// изучение методов
+
+
+
+// =====ВАРИАНТ 1
+// string Method4 (int count, string text)
+// {
+//     int i =0;
+//     string results = String.Empty;
+
+//     while (i<count)
+//     {
+//         results = results + text;
+//         i++;
+//     }
+//     return results;
+// }
+
+
+
+// ======ВАРИАНТ 2
+// string Method4 (int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i =0; i< count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// System.Console.WriteLine(res);
+
+// вывод таблицы умножения
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         System.Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     System.Console.WriteLine();
+// }
+
+
+
+// ===== Работа с текстом
+// Дан тест. В тексе нужно все пробелы заменить черточками, 
+// маленькие буквы "к" заменить большими "К", 
+// а большие "С" заменить маленькими "с"
+
+// string text = "Сеньёр, Я думаю, - сказал князь, улыбаясь, - что, "
+//             + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//             + "вы бы взяли приступом согласие прусского короля. "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// // string s = "qwerty"
+// //             012345
+// // s[3] // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// newText = Replace(newText, 'к', 'К');
+// newText = Replace(newText, 'С', 'с');
+
+// System.Console.WriteLine(newText);
+
+
+
+// =====Упорядочивание данных в массиве (метод сортировки мин/мах)
+//
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+// PrintArray(arr);
+
+// SelectionSort(arr);
+// PrintArray(arr);
+
+
+
+//==== Упорядочивание данных в массиве (метод сортировки мах/мин)
+//
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] > array[minPosition]) minPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+// PrintArray(arr);
+
+// SelectionSort(arr);
+// PrintArray(arr);
