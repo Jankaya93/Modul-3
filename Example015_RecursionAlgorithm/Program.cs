e9db8c2bf11ceb7624261742148b4eb915b7031a
Oksana Oreshkina <jani93@mail.ru>
2023-02-12 12:50:45 +0300

    add lekcia 11/02/2023

---
+++ Example015_RecursionAlgorithm/Program.cs
@@ -0,0 +1,25 @@
+﻿// ===== факториал
+
+// double Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n-1);
+// }
+
+// for (int i = 1; i < 40; i++)
+// {
+//     System.Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+// ===== числа Фибоначчи
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    System.Console.Write(Fibonacci(i) + " ");
+}
